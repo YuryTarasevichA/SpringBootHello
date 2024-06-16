12cee6358b4d6d51a0a1191afd1f72a2ae24a1e9
Yura <yura.tarasevichunt@gmail.com>
2024-06-16 22:28:26 +0300

    HomWork2hello

---
+++ src/test/java/ru/gb/Hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.Hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
